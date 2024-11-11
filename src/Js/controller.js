bd702821183af479674c4eb0c5981084a0200f33
unknown <Lawal223>
2024-11-11 09:55:49 +0100

    Added new welcome feature

--- src/Js/controller.js
+++ src/Js/controller.js
@@ -123,6 +123,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -131,7 +135,6 @@ const init = function () {
   searchView.addHandlerRender(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
-  alert('HACKED');
+  newFeature();
 };
 init();
